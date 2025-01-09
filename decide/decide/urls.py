272342a96a1e48d8f3cfd14a7a861bd21885acfd
Paula Maria Suarez Linares <pausualin>
2025-01-09 18:22:41 +0100

    fix: arreglar error

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
-]
+] 
 
 for module in settings.MODULES:
     urlpatterns += [
